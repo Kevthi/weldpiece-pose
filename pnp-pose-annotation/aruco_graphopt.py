3d83d577b582314015cf851421f4c33746fd1982
Ola Alstad <olaalstad96@gmail.com>
2022-08-16 20:29:38 +0200

    Add progress aruco graphopt

--- pnp-pose-annotation/aruco_graphopt.py
+++ pnp-pose-annotation/aruco_graphopt.py
@@ -1,9 +1,27 @@
+import graphslam
+from graphslam.graph import Graph
+from graphslam.pose.se3 import PoseSE3
+from graphslam.vertex import Vertex
+from graphslam.edge.edge_odometry import EdgeOdometry
+from collections import OrderedDict
+import itertools
 import cv2
 from cv2 import aruco as aruco
 import numpy as np
+from gui_utils import read_rgb, get_image_paths_from_dir
+import matplotlib.pyplot as plt
+import os
+import spatialmath as sm
+from scipy.spatial.transform import Rotation as spR
+from utils_aruco import set_axes_equal, plot_3d_graph, TwoWayDict
+import pickle
+from renderer import render_scene
+import numpy as np
+from debug import nice_print_dict
 
 
-def get_aruko_poses(img, K, aruco_dict, aruko_sq_size):
+def get_aruko_poses(img, K, aruco_dict_str, aruko_sq_size):
+    aruco_dict = aruco.Dictionary_get(getattr(aruco, aruco_dict_str))
     ar_params = aruco.DetectorParameters_create()
     (marker_corners, marker_ids, rejected) = aruco.detectMarkers(img, aruco_dict, parameters=ar_params)
     dist = np.zeros(5)
@@ -12,7 +30,11 @@ def get_aruko_poses(img, K, aruco_dict, aruko_sq_size):
         img = aruco.drawDetectedMarkers(img, marker_corners, marker_ids)
         for single_marker_corner, marker_id in zip(marker_corners, marker_ids):
             rvec,tvec,obj_p_corners = aruco.estimatePoseSingleMarkers(single_marker_corner, aruko_sq_size, K, dist)
-            aruko_poses.append((marker_id, rvec,tvec))
+            T_CA = np.identity(4)
+            R, _ = cv2.Rodrigues(rvec)
+            T_CA[:3,:3] = R
+            T_CA[:3,3] = tvec
+            aruko_poses.append((marker_id, T_CA))
     return aruko_poses
 
 
@@ -29,9 +51,205 @@ def draw_markers(img, K, aruco_dict_str):
             cv2.drawFrameAxes(img, K, dist, rvec,tvec,0.05)
     return img
 
+def add_model_to_poses(img_basename, pose_dict, poses):
+    if "T_CO" in pose_dict[img_basename]:
+        print(img_basename)
+        poses.append(["model", pose_dict[img_basename]["T_CO"]])
+    return poses
 
+
+
+def contains_idx_in_graph(graph, ar_poses):
+    for (marker_idx, T_CA) in ar_poses:
+        if str(marker_idx) in graph:
+            return str(marker_idx), T_CA
+    return None
+
+def init_aruco_graph(graph, img_paths, K, aruco_dict_str, aruko_sq_size):
+
+    graph["[0]"] = np.identity(4)
+
+    graph_changed = True
+    while graph_changed:
+        graph_changed = False
+        for img_path in img_paths:
+            img = read_rgb(img_path)
+            ar_poses = get_aruko_poses(img, K, aruco_dict_str, aruko_sq_size)
+            contained_idx, T_CA = contains_idx_in_graph(graph, ar_poses)
+            if contained_idx:
+                for (marker_idx, T_CA_c) in ar_poses:
+                    if not str(marker_idx) in graph:
+                        print("contained idx", contained_idx, "marker_idx", marker_idx)
+                        graph[str(marker_idx)] = graph[str(contained_idx)]@np.linalg.inv(T_CA)@T_CA_c
+                        graph_changed=True
+    return graph
+
+
+
+
+
+
+def init_aruco_pose_graph(img_paths, K, aruco_dict_str, aruko_sq_size, pose_dict):
+    outgraph = {}
+    outgraph = init_aruco_graph(outgraph, img_paths, K, aruco_dict_str, aruko_sq_size)
+    for img_path in img_paths:
+        img = read_rgb(img_path)
+        poses = get_aruko_poses(img, K, aruco_dict_str, aruko_sq_size)
+        img_basename = os.path.basename(img_path)
+        contained_idx, T_WA = contains_idx_in_graph(outgraph, poses)
+        outgraph["cam_"+img_basename] = outgraph[str(contained_idx)]@np.linalg.inv(T_WA)
+
+        #for (marker_idx, T_CA) in poses:
+            #outgraph[str(marker_idx)] = np.identity(4)
+
+    for img_basename in pose_dict:
+        if "T_CO" in pose_dict[img_basename]:
+            T_CO = pose_dict[img_basename]["T_CO"]
+            outgraph["model"] = outgraph["cam_"+img_basename]@T_CO
+            break
+    return OrderedDict(sorted(outgraph.items()))
+
+
+
+def create_image_pose_dict(img_paths, K, aruco_dict_str, aruko_sq_size):
+    aruco_dict = aruco.Dictionary_get(getattr(aruco, aruco_dict_str))
+    img_paths.sort()
+    img_pose_dict = {}
+    for img_path in img_paths:
+        img = read_rgb(img_path)
+        poses = get_aruko_poses(img, K, aruco_dict_str, aruko_sq_size)
+        img_pose_dict[os.path.basename(img_path)] = poses
+    return OrderedDict(sorted(img_pose_dict.items()))
+
+def T_to_PoseSE3(T):
+    R = T[:3,:3] 
+    t = T[:3,3]
+    q = spR.from_matrix(R).as_quat()
+    T_PoseSE3 = PoseSE3(t, q)
+    return T_PoseSE3
+
+
+def create_graphslam_vertex(idx, T_W):
+    T_W_gs = T_to_PoseSE3(T_W)
+    return Vertex(idx, T_W_gs)
+
+def create_graphslam_edge(idx_k, idx_l, T_kl, info_mat):
+    T_kl_gs = T_to_PoseSE3(T_kl)
+    edge = EdgeOdometry([int(idx_k), int(idx_l)], info_mat, T_kl_gs)
+    return edge
+
+def init_vertices_graphslam(init_graph):
+    id_vert_dict = TwoWayDict()
+    gs_vertices = []
+    for i,marker_idx in enumerate(init_graph):
+        id_vert_dict[marker_idx] = str(i)
+        T_WA = init_graph[marker_idx]
+        gs_vertices.append(create_graphslam_vertex(i, T_WA))
+    return gs_vertices, id_vert_dict
+
+
+
+def create_edges_graphslam(image_pose_dict, id_vert_dict, pose_dict):
+    for key in id_vert_dict:
+        print(key, id_vert_dict[key])
+    gs_edges = []
+    aruko_info_mat = np.identity(6).astype(np.float32)*10.0
+    model_info_mat = np.identity(6).astype(np.float32)*1.0
+    #inf_mat_model = np.identity(6).astype(np.float32)
+    for image_basename in image_pose_dict:
+        image_poses = image_pose_dict[image_basename]
+        image_poses = add_model_to_poses(image_basename, pose_dict, image_poses)
+        for (marker_idx, T_CA) in image_poses:
+            cam_label = "cam_"+image_basename
+            idx_cam = int(id_vert_dict[cam_label])
+            marker_idx = int(id_vert_dict[str(marker_idx)])
+            print("cam_label", cam_label, "mark idx", marker_idx)
+            if marker_idx == 'model':
+                info_mat = model_info_mat
+            else:
+                info_mat = aruko_info_mat
+            edge = create_graphslam_edge(idx_cam, marker_idx, T_CA, info_mat)
+            gs_edges.append(edge)
+    return gs_edges
+    
+
+
+
+
+
+
+def optimize_aruko_graph(init_graph, image_pose_dict, pose_dict):
+    gs_vertices, id_vert_dict = init_vertices_graphslam(init_graph)
+    gs_edges = create_edges_graphslam(image_pose_dict, id_vert_dict, pose_dict)
+    gs_graph = Graph(gs_edges, gs_vertices)
+    gs_graph.optimize(tol=1e-16, max_iter=100)
+    out_graph = {}
+    for vert in gs_graph._vertices:
+        out_graph[id_vert_dict[str(vert.id)]]= vert.pose.to_matrix()
+    return out_graph
+
+
+def get_T_CO(graph, img_basename):
+    T_WC = graph["cam_"+img_basename]
+    T_WO = graph["model"]
+    T_CO = np.linalg.inv(T_WC)@T_WO
+    return T_CO
+
+def aruko_optimize_handler(img_paths, K, aruco_dict_str, aruco_sq_size, pose_dict):
+
+    out_dict = {}
+    init_graph = init_aruco_pose_graph(img_paths, K, aruco_dict_str, aruco_sq_size, pose_dict)
+    img_pose_dict = create_image_pose_dict(img_paths, K, aruco_dict_str, aruco_sq_size)
+    out_graph = optimize_aruko_graph(init_graph, img_pose_dict, pose_dict)
+
+    #fig = plt.figure()
+    #ax1 = fig.add_subplot(1,2,1,projection='3d')
+    #ax2 = fig.add_subplot(1,2,2,projection='3d')
+    #plot_3d_graph(ax1, init_graph)
+    #plot_3d_graph(ax2, out_graph)
+    #set_axes_equal(ax1)
+    #set_axes_equal(ax2)
+    ## EXIT ##
+    #plt.show()
+    for img_path in img_paths:
+        img_basename = os.path.basename(img_path)
+        out_dict[img_basename] = {}
+        out_dict[img_basename]["T_CO_opt"] = get_T_CO(out_graph, img_basename)
+    return out_dict
+    
 
 
 
 if __name__ == '__main__':
-    pass
+    with open('pose_dict.pkl', 'rb') as handle:
+        pose_dict = pickle.load(handle)
+    K = np.array([[1166.3, .0, 509],[0, 1166.0, 546.0],[0,0,1.0]])
+    print(type(K[0,0]))
+    K_load = np.load("K.npy")
+    print(type(K_load[0,0]))
+    print("K load")
+    print(np.load("K.npy"))
+    
+    #print("K_old", K_old)
+    print("K", K)
+    K = K.astype(np.float64)
+    img_dir = "/home/ola/projects/weldpiece-pose-datasets/ds-projects/office-corner-brio/captures"
+    img_paths = get_image_paths_from_dir(img_dir)
+    aruco_dict_str = "DICT_APRILTAG_16H5"
+    aruco_sq_size = 66.0*1e-3
+
+    opt_dict = aruko_optimize_handler(img_paths, K, aruco_dict_str, aruco_sq_size, pose_dict)
+
+    obj_path = "/home/ola/projects/weldpiece-pose-datasets/3d-models/corner.ply"
+
+    nice_print_dict(opt_dict)
+
+    for key in opt_dict:
+        T_CO = opt_dict[key]["T_CO_opt"]
+        print("T_CO render")
+        print(T_CO)
+        img, dep = render_scene(obj_path, T_CO, K, (1080,1080))
+        plt.imshow(img)
+        plt.show()
+
+
--- pnp-pose-annotation/gui_aruco_graphopt.py
+++ pnp-pose-annotation/gui_aruco_graphopt.py
@@ -1,10 +1,18 @@
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.scrollview import ScrollView
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 from gui_components import Sidebar, ColBoxLayout, TextureImage, ColScrollView, SidebarHeader
 from gui_components import color_profile as cp
-from gui_utils import get_image_paths_from_dir, read_rgb
-from aruco_graphopt import draw_markers
+from gui_utils import get_image_paths_from_dir, read_rgb, blend_imgs
+from aruco_graphopt import draw_markers, aruko_optimize_handler
+from gui_utils import get_image_paths_from_dir, merge_dict
+from renderer import render_scene
+import os
 from cv2 import aruco as aruco
+import numpy as np
+from debug import nice_print_dict
 
 
 
@@ -15,21 +23,111 @@ class ArucoGraphoptSidebar(Sidebar):
         super().__init__()
         self.state_dict = state_dict
         self.add_widget(ImgFilesScroll(state_dict))
+        self.add_widget(SolveGraphoptBtn(state_dict))
         self.add_widget(Widget())
+
+class SolveGraphoptBtn(Button):
+    def __init__(self, state_dict):
+        super().__init__(text="Solve graph opt", size_hint=(1.0,None), height=50)
+        self.state_dict = state_dict
+        self.bind(on_press=self.on_solve_graph_opt)
+
+    def on_solve_graph_opt(self, btn_instance=None):
+        self.state_dict["aruco"]["graphopt_is_solved"] = True
+        img_dir = self.state_dict["paths"]["image_dir"]
+        img_paths = get_image_paths_from_dir(img_dir)
+        K = self.state_dict["scene"]["cam_K"]
+        ar_dict_str = "DICT_APRILTAG_16H5"
+        ar_sq_size = 66.0*1e-3
+        pose_dict = self.state_dict["pose_dict"]
+        opt_pose_dict = aruko_optimize_handler(img_paths, K, ar_dict_str, ar_sq_size, pose_dict)
+        nice_print_dict(opt_pose_dict)
+        
+        for key in self.state_dict["pose_dict"]:
+            self.state_dict["pose_dict"][key]["T_CO_opt"] = opt_pose_dict[key]["T_CO_opt"]
+        print(opt_pose_dict)
+        self.state_dict["functions"]["render_aruco_graphopt_display_img"]()
+
+
+
 
 class ImgFilesScroll(BoxLayout):
     def __init__(self, state_dict):
-        super().__init__(orientation='vertical', size_hint=(1.0,None), height=500, padding=[0,5,0,0])
+        super().__init__(orientation='vertical', size_hint=(1.0,None), height=500, padding=[15,5,15,0])
         self.state_dict = state_dict
+        self.state_dict["functions"]["render_aruco_img_select"] = self.render_aruco_img_select
         self.add_widget(SidebarHeader(text="Images", font_size=20))
+
+        self.inner_box = ColBoxLayout(cp.PNP_CORR_WIN_BG, orientation='vertical')
+        self.add_widget(self.inner_box)
+
+        self.scroll_view = ScrollView(do_scroll_y=True, bar_width=20)
+        self.inner_box.add_widget(self.scroll_view)
+        self.grid = GridLayout(cols=1, size_hint_y=None)
+        self.grid.bind(minimum_height=self.grid.setter('height'))
+        self.scroll_view.add_widget(self.grid)
+
+        self.render_aruco_img_select()
+
+
+    def render_aruco_img_select(self):
+        self.grid.clear_widgets()
+        img_dir = self.state_dict["paths"]["image_dir"]
+        image_paths = get_image_paths_from_dir(img_dir)
+        image_paths.sort()
+        selected_img_idx = self.state_dict["aruco"]["selected_img_idx"]
+        selected_img = image_paths[selected_img_idx]
+        for idx,img_path in enumerate(image_paths):
+            is_selected = (idx==selected_img_idx)
+            self.grid.add_widget(ImgSelectScrollItem(self.state_dict, idx, img_path, is_selected))
+
+
+
+
+class ImgSelectScrollItem(BoxLayout):
+    def __init__(self, state_dict, img_idx, img_path, is_selected):
+        super().__init__(orientation='horizontal', size_hint=(1.0,None), height=35, padding=[0,0,0,5])
+        self.state_dict = state_dict
+        self.img_idx = img_idx
+
+        if is_selected:
+            bg_color = cp.AR_SELECTED_IMG_BG
+        else:
+            bg_color = cp.PNP_SIDEBAR_CORR
+
+        self.inner_box = ColBoxLayout(bg_color, size_hint=(1.0,1.0))
+        self.add_widget(self.inner_box)
+
+        
+        self.inner_box.add_widget(SidebarHeader(text=os.path.basename(img_path), font_size=16))
+        select_btn = Button(text="Select", size_hint=(0.2,1.0))
+        select_btn.bind(on_press=self.select_img_cb)
+        self.inner_box.add_widget(select_btn)
+
+    def select_img_cb(self, btn_instance=None):
+        self.state_dict["aruco"]["selected_img_idx"] = self.img_idx
+        self.state_dict["functions"]["render_aruco_img_select"]()
+        self.state_dict["functions"]["update_aruco_detect_display"]()
+        self.state_dict["functions"]["render_aruco_graphopt_display_img"]()
+
+
+
+
 
 
 
 class ArucoGraphoptWorkspace(ColBoxLayout):
     def __init__(self, state_dict):
-        super().__init__(cp.FILESELECT_BG, orientation="horizontal")
+        super().__init__(cp.FILESELECT_BG, orientation="vertical")
         self.state_dict = state_dict
-        self.add_widget(ArucoDetectDisplay(state_dict))
+        hori1 = BoxLayout(orientation='horizontal', size_hint=(1.0,0.5))
+        self.add_widget(hori1)
+        self.add_widget(ColBoxLayout(cp.FS_DELIMITER, size_hint=(1.0,None), height=20))
+        hori2 = BoxLayout(orientation='horizontal', size_hint=(1.0,0.5))
+        self.add_widget(hori2)
+        hori2.add_widget(ArucoOptPoseDisplay(state_dict))
+
+        hori1.add_widget(ArucoDetectDisplay(state_dict))
 
 class ArucoDetectDisplay(TextureImage):
     def __init__(self, state_dict):
@@ -55,6 +153,39 @@ class ArucoDetectDisplay(TextureImage):
 
 
 
+class ArucoOptPoseDisplay(TextureImage):
+    def __init__(self, state_dict):
+        self.state_dict = state_dict
+        rgb_img = np.zeros((1080,1080,3))
+        super().__init__(rgb_img)
+        self.state_dict["functions"]["render_aruco_graphopt_display_img"] = self.render_aruco_graphopt_display_img
+
+    def render_aruco_graphopt_display_img(self):
+        graphopt_is_solved = self.state_dict["aruco"]["graphopt_is_solved"]
+        if graphopt_is_solved:
+            img_dir = self.state_dict["paths"]["image_dir"]
+            image_paths = get_image_paths_from_dir(img_dir)
+            image_paths.sort()
+            selected_img_idx = self.state_dict["aruco"]["selected_img_idx"]
+            selected_img = image_paths[selected_img_idx]
+            selected_img_basename = os.path.basename(selected_img)
+            rgb_img = read_rgb(selected_img)
+            obj_path = self.state_dict["paths"]["selected_model"]
+            K = self.state_dict["scene"]["cam_K"]
+            T_CO = self.state_dict["pose_dict"][selected_img_basename]["T_CO_opt"]
+            img_size = self.state_dict["scene"]["orig_img_size"]
+            rgb, depth = render_scene(obj_path, T_CO, K, img_size)
+            mask = np.where(depth>0, 1, 0)
+            blend_alpha = 0.5
+            overlap = blend_imgs(rgb_img, rgb, mask, blend_alpha)
+            self.update_texture(overlap)
+
+
+
+
+
+
+
 
 
 
@@ -70,4 +201,6 @@ class ArucoGraphoptGUI(BoxLayout):
         self.state_dict = state_dict
         self.add_widget(ArucoGraphoptSidebar(state_dict))
         self.add_widget(ArucoGraphoptWorkspace(state_dict))
+        self.state_dict["functions"]["render_aruco_graphopt_display_img"]()
+
 
