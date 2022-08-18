73ea5a80a46e74db82662759300e5a4571125e05
Ola Alstad <olaalstad96@gmail.com>
2022-08-18 22:24:01 +0200

    Add charuko rel pose test for cam calib

--- aruko-pose/charuko-board/run.py
+++ aruko-pose/charuko-board/run.py
@@ -4,76 +4,114 @@ from cv2 import aruco
 import numpy as np
 import matplotlib.pyplot as plt
 
-cam_conf = {
-    "valid_frame_sizes": {
-        "3840x2160": (8000,8000),
-        "1920x1080": (1920,1080),
-        "1280x720": (1280,720),
-        "1500x1500":(1500,1500),
-        "640x480": (640,480),
-        "320x240": (320,240),
-        "160x120": (160,120)
-    },
-    "capture_resolution": "1920x1080",
-    "focus_distance": 0,
-    "use_autofocus": False,
-    "crop_size": (1080,1080),
-    "resize_to": None,
-}
-K = np.load("K.npy")
-dist = np.load("dist_coeffs.npy")
 
 def create_board(squares_x, squares_y, cb_sq_width, aruco_sq_width, aruco_dict_str, start_id):
     aruco_dict = aruco.Dictionary_get(getattr(aruco, aruco_dict_str))
     aruco_dict.bytesList=aruco_dict.bytesList[start_id:,:,:]
-    board = aruco.CharucoBoard_create(squares_x,squares_y,1,aruco_sq_width,aruco_dict)
+    board = aruco.CharucoBoard_create(squares_x,squares_y,cb_sq_width,aruco_sq_width,aruco_dict)
     return board, aruco_dict
 
 
 
 
-cam =WebcamHandler(0, cam_conf)
 
 #aruco_dict = cv2.aruco.Dictionary_get(aruco.DICT_5X5_250)
 #aruco_dict.bytesList=aruco_dict.bytesList[30:,:,:]
 #board = aruco.CharucoBoard_create(6, 8, 32.521*1e-3, 32.521*1e-3/2.0, aruco_dict)
-board, aruco_dict = create_board(4,3, 37.5*1e-3, 24.375*1e-3, "DICT_APRILTAG_16H5", 6)
 #plt.imshow(board.draw((500,500)))
 #plt.show()
 
-def detect_charuko(cam, board, aruco_dict, win_name):
-    i = 0
+def draw_charuco_corners(img, board, aruco_dict):
+    dist = np.zeros(5)
+    img = img.copy()
     ar_params = cv2.aruco.DetectorParameters_create()
+    (marker_corners, marker_ids, rejected) = aruco.detectMarkers(img, aruco_dict, parameters=ar_params)
+    if marker_ids is not None and len(marker_ids) > 0:
+        img = aruco.drawDetectedMarkers(img, marker_corners, marker_ids)
+        num, char_corners, char_ids = aruco.interpolateCornersCharuco(marker_corners, marker_ids, img, board, cameraMatrix=K, distCoeffs=dist)
+        if(char_ids is not None and len(char_ids)>0):
+            aruco.drawDetectedCornersCharuco(img, char_corners, char_ids, (0,0,255))
+    return img
+
+
+def get_charuco_pose(img, board, K, aruco_dict):
+    dist = np.zeros(5)
+    ar_params = cv2.aruco.DetectorParameters_create()
+    (marker_corners, marker_ids, rejected) = aruco.detectMarkers(img, aruco_dict, parameters=ar_params)
+    rvec = None
+    tvec = None
+    valid = False
+    if marker_ids is not None and len(marker_ids) > 0:
+        num, char_corners, char_ids = aruco.interpolateCornersCharuco(marker_corners, marker_ids, img, board, cameraMatrix=K, distCoeffs=dist)
+        if(char_ids is not None and len(char_ids)>0):
+            rvec = np.array([0,0,0])
+            tvec = np.array([0,0,0])
+            valid, rvec,tvec = aruco.estimatePoseCharucoBoard(char_corners, char_ids, board, K, dist, np.empty(1), np.empty(1))
+
+    return valid, rvec,tvec
+
+def cv2_pose_to_T(rvec,tvec):
+    T_CA = np.identity(4)
+    R, _ = cv2.Rodrigues(rvec)
+    T_CA[:3,:3] = R
+    T_CA[:3,3] = tvec.flatten()
+    return T_CA
+
+
+def detect_charuko(cam, K, dist_coeffs, win_name):
+    i = 0
+    board_id0, aruco_dict_id0 = create_board(4,3, 37.5*1e-3, 24.375*1e-3, "DICT_APRILTAG_16H5", 0)
+    board_id6, aruco_dict_id6 = create_board(4,3, 37.5*1e-3, 24.375*1e-3, "DICT_APRILTAG_16H5", 6)
+
+
 
     while(True):
         ret, img = cam.get_camera_image()
-        show_img = img
-        img_copy = img.copy()
 
-        (marker_corners, marker_ids, rejected) = aruco.detectMarkers(img_copy, aruco_dict, parameters=ar_params)
-        if marker_ids is not None and len(marker_ids) > 0:
-            img_copy = aruco.drawDetectedMarkers(img_copy, marker_corners, marker_ids)
-            num, char_corners, char_ids = aruco.interpolateCornersCharuco(marker_corners, marker_ids, img, board, cameraMatrix=K, distCoeffs=dist)
-            if(char_ids is not None and len(char_ids)>0):
-                aruco.drawDetectedCornersCharuco(img_copy, char_corners, char_ids, (0,0,255))
-                rvec = np.array([0,0,0])
-                tvec = np.array([0,0,0])
-                valid, rvec,tvec = aruco.estimatePoseCharucoBoard(char_corners, char_ids, board, K, dist, np.empty(1), np.empty(1))
-                if(valid):
-                    print("valid")
-                    cv2.drawFrameAxes(img_copy, K, dist, rvec,tvec, 1.0)
-                    show_img = img_copy
-        print(i)
-        cv2.imshow("show_img", show_img)
+        img = cv2.undistort(img, K, dist_coeffs)
+
+        valid_0,rvec,tvec = get_charuco_pose(img, board_id0, K, aruco_dict_id0)
+        if(valid_0):
+            T_C0 = cv2_pose_to_T(rvec,tvec)
+            cv2.drawFrameAxes(img, K, dist, rvec,tvec, 0.15)
+            draw_charuco_corners(img, board_id0, aruco_dict_id0)
+
+
+        valid_1,rvec,tvec = get_charuco_pose(img, board_id6, K, aruco_dict_id6)
+        if(valid_1):
+            T_C1 = cv2_pose_to_T(rvec,tvec)
+            cv2.drawFrameAxes(img, K, dist, rvec,tvec, 0.15)
+            draw_charuco_corners(img, board_id0, aruco_dict_id6)
+
+        if valid_0 and valid_1:
+            T_01 = np.linalg.inv(T_C0)@T_C1
+            print(T_01[:3,3])
+
+
+
+
+        img = cv2.resize(img, (1080,1080))
+        cv2.imshow("show_img", img)
         i+=1
         if cv2.waitKey(1)& 0xFF ==ord("q"):
             break
 
 
-board_id0, aruco_dict_id0 = create_board(4,3, 37.5*1e-3, 24.375*1e-3, "DICT_APRILTAG_16H5", 0)
+if __name__ == '__main__':
+    import json
+    with open('brio-2160.json') as json_file:
+        cam_conf  = json.load(json_file)
+    print(cam_conf)
+    cam =WebcamHandler(0, cam_conf)
+    K = np.load("K.npy")
+    print("K")
+    print(K)
+    dist = np.load("dist_coeffs.npy")
+    print("dist coeffs")
+    print(dist)
 
-detect_charuko(cam, board, aruco_dict, "board idx 6")
-detect_charuko(cam, board_id0, aruco_dict_id0, "board idx 0")
+    #detect_charuko(cam, board, aruco_dict, "board idx 6")
+    detect_charuko(cam, K, dist, "board idx 0")
 
 
         
